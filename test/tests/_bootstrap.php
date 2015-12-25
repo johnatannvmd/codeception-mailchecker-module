8f4b10fbea3e6afba1411fa934aabe4b08e29724
johnatannvmd <webmaster@johnatan.ru>
2015-12-25 23:52:22 +0600

    Move providers config to evn aware place.

--- test/tests/_bootstrap.php
+++ test/tests/_bootstrap.php
@@ -3,3 +3,39 @@ require_once 'acceptance/BaseMailChecker.php';
 require_once '_support/BaseMailerHelper.php';
 
 date_default_timezone_set('Etc/GMT');
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['providers'] = [];
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['providers']['MailDump'] = [
+    'options' => [
+        'url' => getenv('MAIL_SERVICE_HOST'),
+        'port' => '1080'
+    ],
+    'smtpHost' => '127.0.0.1',
+    'smtpPort' => '1025'
+];
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['providers']['MailCatcher'] = [
+    'options' => [
+        'url' => getenv('MAIL_SERVICE_HOST'),
+        'port' => '1081'
+    ],
+    'smtpHost' => '127.0.0.1',
+    'smtpPort' => '1026'
+];
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['providers']['LatherMail'] = [
+    'options' => [
+        'url' => getenv('MAIL_SERVICE_HOST'),
+        'port' => '1082'
+    ],
+    'smtpHost' => '127.0.0.1',
+    'smtpPort' => '1027'
+];
+
+\Codeception\Configuration::$defaultSuiteSettings['modules']['config']['providers']['ZendMail'] = [
+    'options' => [
+        'path' => 'tests/_output',
+        'extension' => 'eml'
+    ]
+];
